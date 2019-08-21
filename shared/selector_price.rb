e42fc371ca732abadd878635fa042f7c18f19b52
Diane Douglas <ddouglas@weblinc.com>
2019-08-21 17:14:07 -0400

    Add condition for tests.

--- shared/selector_price.rb
+++ shared/selector_price.rb
@@ -1,14 +1,15 @@
+# Price Selector
 # Selects items in the cart that are greater than (or less than) a price.
-#
-# Example
-# -------
-#   * Items with a price greater than $5
-#   PriceSelector.new(:greater_than, Money.new(cents: 5_00))
-#
+
 class PriceSelector
 
   def initialize(condition, price)
-    @price = price
+    if $ENV && $ENV['TEST_ENV']
+      # tests have to use an older Money gem because the shopify scripts one isn't documented.
+      @price = Money.new(price)
+    else
+      @price = Money.new(cents: price)
+    end
     @condition = condition
   end
 
@@ -22,3 +23,10 @@ class PriceSelector
   end
 end
 
+# Usage:
+# Items with a price greater than $5
+# PriceSelector.new(:greater_than, 500)
+
+# PriceSelector takes 2 inputs.
+# 1) condition - symbol - either :greater_than or :less_than
+# 2) price - integer - the number of cents to compare the line item price.
