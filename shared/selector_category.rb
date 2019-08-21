02cef3775e7b5f8453fe660ba8b60dadcc7d009d
Diane Douglas <ddouglas@weblinc.com>
2019-08-21 17:13:43 -0400

    Fix a bug with category selector where we want to always return true if there are no tags to match.

--- shared/selector_category.rb
+++ shared/selector_category.rb
@@ -1,12 +1,6 @@
+# Category Selector
 # Selects items by 1 or more tags.
-#
-# Example
-# -------
-#   * Items tagged with "sale" or "new".
-#   CategorySelector.new(['sale', 'new'])
-#   CategorySelector.new(['featured'])
-#
-#
+
 class CategorySelector
 
   #    Array of strings that the selector will look for in the item tags.
@@ -16,7 +10,16 @@ class CategorySelector
 
   def match?(line_item)
     # take each tag on the line item, and if any of them are included in the category tags return true.
+    return true if @category_tags.empty?
     line_item.variant.product.tags.any?{ |tag| @category_tags.include?(tag) }
   end
 end
 
+# Usage:
+# Select items tagged with "sale" or "new".
+# CategorySelector.new(['sale', 'new'])
+# CategorySelector.new(['featured'])
+# CategorySelector.new([])
+
+# CategorySelector takes 1 input
+# 1) array of tags to look for on the given line_item. if any product tags match, return true.
