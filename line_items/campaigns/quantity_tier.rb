106e4f632b150922052be74b0607b0e55f39dc62
Diane Douglas <ddouglas@weblinc.com>
2019-08-21 17:12:16 -0400

    Cleanup comments, fix extra message argument on spendx, use category selector instead of doing it inline and refactor.

--- line_items/campaigns/quantity_tier.rb
+++ line_items/campaigns/quantity_tier.rb
@@ -1,4 +1,6 @@
-# BUY MORE SAVE MORE: X QTY FOR $Y
+# QuantityTier Campaign
+# Buy more save more.
+# Apply a discount to a line item based on its quantity. 
 
 class QuantityTierCampaign
   attr_reader :coupon_code
@@ -33,23 +35,18 @@ class QuantityTierCampaign
 end
 
 # Usage:
-#
-# # Tag products for tiered discount campaign. Optional. 
-# # Without tags, any item triggers the discount when bought in enough quantity.
-# TAGS = ['BUYXQTY']
+# Buy at least 10, get 10% off. Buy 20, get 20% off.
 
-# # quantity => discount type with price and message.
-# # Use flat rate and or percent discount for any tier with any message.
-
-# # Flat Rate example: 
-# # DISCOUNTS_BY_QUANTITY = {
-# #   40 => SetFlatAmountDiscount.new(Money.new(cents: 1_00), 'Buy 40 for $1!'),
-# #   30 => SetFlatAmountDiscount.new(Money.new(cents: 2_00), 'Buy 30 for $2!'),
-# #   20 => SetFlatAmountDiscount.new(Money.new(cents: 3_00), 'Buy 20 for $3!'),
-# #   10 => SetFlatAmountDiscount.new(Money.new(cents: 4_00), 'Buy 10 for $4!'),
-# # }
-
-# # Percentage discount example
+# QuantityTierCampaign needs 3 inputs.
+# 1) discounts_by_quantity is an object defining the quantity tiers needed to qualify. For example: 
+# Flat Rate example: 
+# DISCOUNTS_BY_QUANTITY = {
+#   40 => SetFlatAmountDiscount.new(Money.new(cents: 1_00), 'Buy 40 for $1!'),
+#   30 => SetFlatAmountDiscount.new(Money.new(cents: 2_00), 'Buy 30 for $2!'),
+#   20 => SetFlatAmountDiscount.new(Money.new(cents: 3_00), 'Buy 20 for $3!'),
+#   10 => SetFlatAmountDiscount.new(Money.new(cents: 4_00), 'Buy 10 for $4!'),
+# }
+# Percent example:
 # DISCOUNTS_BY_QUANTITY = {
 #   50 => PercentageDiscount.new(50, 'Buy 50, get 50% off!'),
 #   30 => PercentageDiscount.new(30, 'Buy 30, get 30% off!'),
@@ -57,10 +54,5 @@ end
 #   10 => PercentageDiscount.new(10, 'Buy 10, get 10% off!')
 # }
 
-# CAMPAIGNS << QuantityTierCampaign.new(
-#   DISCOUNTS_BY_QUANTITY,
-#   [
-#     CategorySelector.new(TAGS),
-#   ]
-# )
-
+# 2) An array [] of conditions to define your category (a CategorySelector for tags, a PriceSelector to restrict by price etc.)
+# 3) Optional coupon code to require.
--- line_items/campaigns/spend_x_save.rb
+++ line_items/campaigns/spend_x_save.rb
@@ -1,12 +1,12 @@
 # SPEND $X SAVE $Y
+# If the qualifying items in the cart cost more than the threshold apply a discount evenly to them.
 
 class SPENDXSAVECampaign
   attr_reader :coupon_code
 
-  def initialize(spend_threshold, discount, message, discount_tags = [], code = nil, once = false)
+  def initialize(spend_threshold, discount, discount_tags = [], code = nil, once = false)
     @spend_threshold = spend_threshold
     @discount = discount
-    @message = message
     @discount_tags = discount_tags
     @coupon_code = CouponCode.new(code) if code
     @once = once
@@ -20,49 +20,60 @@ class SPENDXSAVECampaign
     eligible_items = Input.cart.line_items.select do |line_item|
       # if eligible, put the line_item in the array and add its price to total_cart_price.
       # replace with CategorySelector
-      if @discount_tags.empty? || @discount_tags.any?{ |tag| product.tags.include?(tag) }
+      @category_selector = CategorySelector.new(@discount_tags)
+      if @category_selector.match?(line_item)
         total_cart_price += line_item.line_price.cents
       end
     end
     
-    puts "eligible_items #{eligible_items}"
     return if eligible_items.empty? || total_cart_price < @spend_threshold
 
     case @discount.class.to_s
     when 'PercentageDiscount'
-      puts "inside of case percentage discount"
       eligible_items.each do |line_item|
-        puts "applying discount to line_item"
         @discount.apply(line_item)
       end
     when 'SetFlatAmountDiscount'
-      puts "inside of SetFlatAmountDiscount"
       if @once
-        puts "inside of SetFlatAmountDiscount once"
         # buy $50 get $10 back, but it does not compound. just distribute over items once.
         total_discount = Decimal.new(@discount.amount.cents)
       else
-        puts "inside of SetFlatAmountDiscount compounding"
         # Distribute the total discount across the products propotional to their price
         # calculate total_discount based on the cart price and how much it exceeds the threshold.
-        total_discount = (total_cart_price/@spend_threshold).floor * Decimal.new(@discount.amount.cents)
+        times_to_apply = (total_cart_price/@spend_threshold).floor
+        amount_to_apply = Decimal.new(@discount.amount.cents)
+        total_discount = times_to_apply * amount_to_apply
       end
       remainder = Decimal.new(0)
       eligible_items.each do |line_item|
+        # price of line_item including quantity
         price = line_item.line_price.cents
-        # money / decimal probably coverts proportion to money
+        # how much of the total cart price is it? we distribute the total_discount evenly.
         proportion =  Decimal.new(price / total_cart_price)
-        # multiply total_discount by proportion for this item. 
-        # add remainder - it will initially be 0.
+        # multiply total_discount by proportion for each item and add remainder (initially is 0). 
         discount_float = (total_discount * proportion) + remainder
         # round to nearest.
         discount = discount_float.round
         # get remainder to pass to next
         remainder =  discount_float - discount
-        line_item.change_line_price(line_item.line_price - Money.new(cents: discount), message: @message) unless discount == 0
+        # set price to current - calculated discount
+        line_item.change_line_price(line_item.line_price - Money.new(cents: discount), message: @discount.message) unless discount == 0
       end
     else
       return
     end
   end
 end
+
+# Usage:
+# Spend $50 get $5 back.
+
+# SPENDXSAVECampaign takes 5 inputs.
+# 1) Spend threshold - integer - cents to qualify for discount
+# 2) A discount to apply (flat rate, percentage, etc.)
+# 3) Optional: An array [] of conditions to define your category (a CategorySelector for tags, a PriceSelector to restrict by price etc.)
+# 4) Optional: coupon code to require.
+# 5) Optional: Boolean value to decide whether or not to discount a flat rate multiple times. 
+#    If once is true, then if you exceed the threshold you only get the discount once.
+#    If once is false, then you get the discount every time the threshold is exceeded. 
+#    If we are using a percent discount then once is ignored. Percents are applied only once. 
